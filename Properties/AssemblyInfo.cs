5f8f9019fa71009d4b9cade0a217948f3f391be2
Data Juggler <corby@datajuggler.com>
2020-01-09 14:17:29 -0600

    I am checking in the first version for this library. This code previously existed in DataJuggler.Core.UltimateHelper but it added many references so I moved it to its own library.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataJuggler.Net.Cryptography")]
+[assembly: AssemblyDescription("This package is a port of DataJuggler.Core.Cryptography for dot net core. It uses using Konscious.Security.Cryptography.Argon2 by Keef Aragaon.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Data Juggler")]
+[assembly: AssemblyProduct("DataJuggler.Net.Cryptography")]
+[assembly: AssemblyCopyright("Copyright ©  2020 - Free To Use For Whatever You Want")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("889d2fd7-3acb-4a8e-8c38-2615cffb835f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
